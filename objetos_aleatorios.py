d7c8cb3ea31acbadff300d68fa85027e48223464
jonatan javier garzon nieto <jonatangarzon@colegioguanenta.edu.co>
2026-08-27 16:27:48 -0500

    Add files via upload

--- objetos_aleatorios.py
+++ objetos_aleatorios.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 #---------------------
 # Variables globales
@@ -29,6 +30,44 @@ frame_graficacion.place(x=10, y=10)
 c = Canvas(frame_graficacion, width=BASE, height=ALTURA)
 c.config(bg="#B7E9BF")
 c.place(x=10, y=10)
+
+#--------------------------------
+# dibujar n objetos de tamaño y objetos aleatorios
+#----------------------------------
+# lineas rectas
+for i in range(50):
+    # punto inicialde la linea
+    x1 = random.randint(10, int(BASE//2))
+    y1 = random.randint(10, ALTURA)
+    # punto final de la linea
+    X2 = random.randint(10, int(BASE/2))
+    Y2 = random.randint(10, ALTURA)
+
+    # generar un color aleatorio
+    color = "#"
+
+    for caracter in range(6):
+        color = color + random.choice("0123456789ABCDEF")
+    # Dibujar la linea
+    linea = c.create_line(x1, y1, X2, Y2, fill=color, width=2)
+
+# circulos de tamaño 20x20
+for i in range(50):
+    # punto inicial de la linea
+    x1 = random.randint( int(BASE/2), BASE-20)
+    y1 = random.randint(0, ALTURA-20)
+    # punto final de la linea
+    X2 = x1 + 20
+    Y2 = y1 + 20
+
+    # generar un color aleatorio
+    color = "#"
+
+    for caracter in range(6):
+        color = color + random.choice("0123456789ABCDEF")
+    # Dibujar la linea
+    circulo = c.create_oval(x1, y1, X2, Y2, fill=color, width=2)    
+
 
 #----------------------------------
 # frame de cpontroles   
